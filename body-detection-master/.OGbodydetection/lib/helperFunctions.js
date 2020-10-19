4bcf73f14148fb8f00ca8ad343963d3efa6c9d46
Kreosot <max.derlow@gmail.com>
2020-10-19 10:41:10 +0200

    OG library, no changes made, no changes to make.

--- body-detection-master/.OGbodydetection/lib/helperFunctions.js
+++ body-detection-master/.OGbodydetection/lib/helperFunctions.js
@@ -4,6 +4,23 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
+//x = distance between the user's eyes
 function distanceCm(x) {
     return -21.6 + (594.6724 - -21.6)/(1 + (x/8.436912)**1.09424);
+}
+
+function draw(bodyPart, colour, radius){
+    if(!bodyPart){return;} //jesus fuck this syntax
+
+    let x = bodyPart.position.x;
+    let y = bodyPart.position.y;
+    let confidenceScore = bodyPart.confidenceScore;
+
+    if(confidenceScore >= 0.1) {
+        //console.log("drawing: " + bodyPart + " with confidenceScore of: " + confidenceScore);
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = colour;
+        ctx.fill();
+    }
 }
