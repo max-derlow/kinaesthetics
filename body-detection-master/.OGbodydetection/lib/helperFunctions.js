1fd810465db709e3463a911fc52bca07c14807da
Kreosot <max.derlow@gmail.com>
2020-10-20 11:37:20 +0200

    added a visual lib. 'Index' should be connected with our stuff

--- body-detection-master/.OGbodydetection/lib/helperFunctions.js
+++ body-detection-master/.OGbodydetection/lib/helperFunctions.js
@@ -8,7 +8,7 @@ function getRandomInt(min, max) {
 function distanceCm(x) {
     return -21.6 + (594.6724 - -21.6)/(1 + (x/8.436912)**1.09424);
 }
-
+/*
 function draw(bodyPart, colour, radius){
     if(!bodyPart){return;} //jesus fuck this syntax
 
@@ -16,11 +16,12 @@ function draw(bodyPart, colour, radius){
     let y = bodyPart.position.y;
     let confidenceScore = bodyPart.confidenceScore;
 
-    if(confidenceScore >= 0.1) {
-        //console.log("drawing: " + bodyPart + " with confidenceScore of: " + confidenceScore);
+    if(confidenceScore >= 0) {
         ctx.beginPath();
         ctx.arc(x, y, radius, 0, 2 * Math.PI);
         ctx.fillStyle = colour;
         ctx.fill();
-    }
-}
+    }*/
+
+
+
